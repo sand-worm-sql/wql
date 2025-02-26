b9fe36f225277e211071c36714d0ca0331cf10d2
Angry Coding <mailidanielifechukwu@gmail.com>
2025-02-26 08:42:04 +0000

    chore: executor

---
+++ core/src/executor/mod.rs
@@ -0,0 +1,30 @@
+use {
+    // self::function::BreakCase,
+    // super::{context::RowContext, select::select},
+    crate::{
+        ast::{Aggregate, Expr, Function},
+        // data::{CustomFunction, Interval, Literal, Row, Value},
+        // mock::MockStorage,
+        result::{Error, Result}, 
+        // store::GStore,
+    },
+    async_recursion::async_recursion,
+    chrono::prelude::Utc,
+    futures::{
+        future::{ready, try_join_all},
+        stream::{self, StreamExt, TryStreamExt},
+    },
+    im_rc::HashMap,
+    std::{borrow::Cow, ops::ControlFlow, rc::Rc},
+};
+
+
+
+mod aggregate;
+
+
+pub use aggregate::AggregateError;
+
+pub async fn evaluate_stateless(expr: Expr) -> Result<()> {
+    Ok(())
+}
