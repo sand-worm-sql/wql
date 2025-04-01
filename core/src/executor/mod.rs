0816b8e8086dc27df7bc29603499fadb96934618
Angry Coding <mailidanielifechukwu@gmail.com>
2025-04-01 03:25:24 +0000

    chore:fixes

--- core/src/executor/mod.rs
+++ core/src/executor/mod.rs
@@ -1,7 +1,5 @@
 mod aggregate;
-mod alter;
 mod context;
-mod delete;
 mod evaluate;
 mod execute;
 mod fetch;
@@ -11,20 +9,16 @@ mod join;
 mod limit;
 mod select;
 mod sort;
-mod update;
 mod validate;
 
 pub use {
     aggregate::AggregateError,
-    alter::{AlterError, Referencing},
     context::RowContext,
-    delete::DeleteError,
     evaluate::{evaluate_stateless, EvaluateError},
     execute::{execute, ExecuteError, Payload, PayloadVariable},
     fetch::FetchError,
     insert::InsertError,
     select::SelectError,
     sort::SortError,
-    update::UpdateError,
     validate::ValidateError,
 };
