a1fa699034fd8693bf4cee47202dbbe5b502ba75
Angry Coding <mailidanielifechukwu@gmail.com>
2025-04-22 08:00:24 +0000

    chore: evm adapter

--- chains-support/evm-adapter/src/result.rs
+++ chains-support/evm-adapter/src/result.rs
@@ -1,12 +1,25 @@
 use {serde::Serialize, std::fmt::Debug, thiserror::Error as ThisError};
 
-pub use crate::{
- 
+pub use crate::common::{
+    chain::EvmChainError,
+    entity_id::EntityIdError,
+    entity::EntityError,
+    account::AccountError
 };
 
 #[derive(ThisError, Serialize, Debug, PartialEq)]
 pub enum Error {
- 
+    #[error("evm: {0}")]
+    EvmChainError(EvmChainError),
+
+    #[error("entity_id: {0}")]
+    EntityIdError(EntityIdError),
+
+    #[error("entity: {0}")]
+    EntityError(EntityError),
+
+    #[error("error: {0}")]
+    AccountError(AccountError)
 }
 
 pub type Result<T, E = Error> = std::result::Result<T, E>;
