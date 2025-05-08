332f1f27cc2953c4cb714747b0de63a0828108b0
Angry Coding <mailidanielifechukwu@gmail.com>
2025-05-08 11:25:59 +0000

    chore: change adaters

--- chains-support/sui-adapter/src/lib.rs
+++ chains-support/sui-adapter/src/lib.rs
@@ -1,20 +1,2 @@
-mod common;
 //mod resolvers;
-mod result;
 
-pub mod prelude {
-    pub use crate::{
-        // ast::DataType,
-        // data::{Key, Row, Value},
-        // executor::{execute, Payload, PayloadVariable},
-        // parse_sql::parse,
-        // plan::plan,
-        result::{Error, Result},
-        // translate::translate,
-        // worm::Worm,
-    };
-}
-
-pub mod error {
-    pub use crate::result::*;
-}
